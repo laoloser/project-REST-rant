e3a693051c3bf9831b7de4ede2029156fc34db96
laoloser <slaux86@gmail.com>
2024-05-09 15:56:12 -0700

    adding home

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('error404')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
