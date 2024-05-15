9cfe59313e69430c3e49372f4305d32a5b1784be
laoloser <slaux86@gmail.com>
2024-05-15 00:03:00 -0700

    Added MongoDB

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,12 +1,12 @@
-const React = require('react')
-const Def = require('../default')
+const React = require('react');
+const Def = require('../default');
 
-function index (data) {
-  let placesFormatted = data.places.map((place, index) => {
+function index({ places }) {
+  let placesFormatted = places.map((place) => {
     return (
-      <div className="col-sm-6">
+      <div key={place.id} className="col-sm-6">
         <h2>
-          <a href={`/places/${index}`} >
+          <a href={`/places/${place.id}`} >
             {place.name}
           </a>
         </h2>
@@ -18,21 +18,18 @@ function index (data) {
           Located in {place.city}, {place.state}
         </p>
       </div>
-    )
-  })
+    );
+  });
   return (
     <Def>
         <main>
             <h1>THESE PLACES ARE FIRE</h1>
             <div className="row">
-            {placesFormatted}
+              {placesFormatted}
             </div>
         </main>
     </Def>
-)
+  );
 }
 
-  
-
-module.exports = index
-
+module.exports = index;
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -25,7 +25,16 @@ function new_form() {
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required />
+            <input
+              className="form-control"
+              id="cuisines"
+              name="cuisines"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label for="founded">Founded Year</label>
+            <input className="form-control" id="founded" name="founded" />
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
