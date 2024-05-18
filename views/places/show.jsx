9e6da1cfa771a8afc282ba4f677ca61af55972a4
laoloser <slaux86@gmail.com>
2024-05-17 18:45:58 -0700

    Added Validation

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,7 +6,17 @@ function show(data) {
     <Def>
       <main>
         <h1>{ data.place.name }</h1>
-        <div>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name}/>
+            <h3>Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+          </div>
           <h2>Rating</h2>
           <p>Not Rated</p>
         </div>
